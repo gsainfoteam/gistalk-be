f846111bd333776a74a83e65c1fb2d2f558b1a78
siwonpada <psw041115@gmail.com>
2024-06-19 00:40:01 +0900

    enable usermoduel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { ProfessorController } from './professor/professor.controller';
+import { ProfessorModule } from './professor/professor.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ProfessorModule,
+    UserModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, ProfessorController],
 })
 export class AppModule {}
