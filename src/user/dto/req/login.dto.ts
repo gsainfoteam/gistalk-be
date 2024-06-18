11f07964d375b43c74126a55e84716d6ac7085ef
siwonpada <psw041115@gmail.com>
2024-06-19 01:07:02 +0900

    update swagger

--- src/user/dto/req/login.dto.ts
+++ src/user/dto/req/login.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { LoginType, loginEnum } from 'src/user/types/login.type';
 
 export class LoginDto {
+  @ApiProperty({
+    example: 'code',
+    description: 'idp code',
+  })
   @IsString()
   @IsNotEmpty()
   code: string;
 
+  @ApiProperty({
+    example: 'web local flutter',
+    description: 'idp type',
+  })
   @IsString()
   @IsEnum(loginEnum)
   @IsOptional()
--- src/user/dto/req/logout.dto.ts
+++ src/user/dto/req/logout.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class LogoutDto {
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+    description: 'access token',
+  })
   @IsString()
   access_token: string;
 }
