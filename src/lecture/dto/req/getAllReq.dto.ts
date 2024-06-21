b6fa34286f2fafdb6c61c3d16045956369b2cf92
siwonpada <psw041115@gmail.com>
2024-06-21 22:02:59 +0900

    update: swagger

--- src/lecture/dto/req/getAllReq.dto.ts
+++ src/lecture/dto/req/getAllReq.dto.ts
@@ -2,7 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class GetAllQueryDto {
-  @ApiProperty()
+  @ApiProperty({
+    required: false,
+  })
   @IsString()
   @IsOptional()
   professorName?: string;
