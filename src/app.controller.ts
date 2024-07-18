daf842ab12a9c59d15dfb857f84277d8ba4ced8b
Shiwon Park <psw041115@gmail.com>
2024-07-18 20:14:44 +0900

    feat: api root and workflow

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 @Controller()
 export class AppController {
+  private readonly apiVersion =
+    this.configService.getOrThrow<string>('API_VERSION');
+  private readonly publishedAt = new Date().toISOString();
+  constructor(private readonly configService: ConfigService) {}
+
   @Get()
-  getHello(): string {
-    return 'pong';
+  getHello() {
+    return {
+      name: 'Gistalk',
+      version: this.apiVersion,
+      publishedAt: this.publishedAt,
+    };
   }
 }
