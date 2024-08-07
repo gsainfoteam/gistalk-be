13e4d5c6c7aa82b1857969a16832f6fab66b959e
controlZ <o_dahyun@gm.gist.ac.kr>
2024-08-07 10:25:41 +0900

    ♻️ refactor: merge migration file

--- prisma/migrations/20240807012120_add_lecture_section_info/migration.sql
+++ prisma/migrations/20240807012120_add_lecture_section_info/migration.sql
@@ -1,21 +1,9 @@
-/*
-  Warnings:
-
-  - You are about to drop the `course_registration_rate` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "course_registration_rate" DROP CONSTRAINT "course_registration_rate_lecture_id_section_id_fkey";
-
--- DropTable
-DROP TABLE "course_registration_rate";
-
 -- CreateTable
 CREATE TABLE "lecture_section_info" (
     "lecture_id" INTEGER NOT NULL,
     "section_id" INTEGER NOT NULL,
-    "capacity" INTEGER NOT NULL,
     "year" INTEGER NOT NULL,
+    "capacity" INTEGER NOT NULL,
     "registration_count" INTEGER NOT NULL,
     "full_capacity_time" INTEGER NOT NULL,
 
