5dc29c28828d11f3847154951211a830928264f8
controlZ <o_dahyun@gm.gist.ac.kr>
2024-08-08 17:33:05 +0900

    🐛 fix: lecture section에 매핑되어있는 key 순서 수정

--- prisma/migrations/20240808082519_modify_lecture_section/migration.sql
+++ prisma/migrations/20240808082519_modify_lecture_section/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - The primary key for the `lecture_section` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the `lecture_section_info` table. If the table is not empty, all the data it contains will be lost.
   - Added the required column `semester` to the `bookmark` table without a default value. This is not possible if the table is not empty.
   - Added the required column `year` to the `bookmark` table without a default value. This is not possible if the table is not empty.
   - Added the required column `capacity` to the `lecture_section` table without a default value. This is not possible if the table is not empty.
@@ -16,9 +15,6 @@
 */
 -- DropForeignKey
 ALTER TABLE "bookmark" DROP CONSTRAINT "bookmark_lecture_id_section_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "lecture_section_info" DROP CONSTRAINT "lecture_section_info_lecture_id_section_id_fkey";
 
 -- DropForeignKey
 ALTER TABLE "lecture_section_professor" DROP CONSTRAINT "lecture_section_professor_section_id_lecture_id_fkey";
@@ -43,14 +39,11 @@ ADD CONSTRAINT "lecture_section_pkey" PRIMARY KEY ("id", "lecture_id", "year", "
 ALTER TABLE "lecture_section_professor" ADD COLUMN     "semester" "Semester" NOT NULL,
 ADD COLUMN     "year" INTEGER NOT NULL;
 
--- DropTable
-DROP TABLE "lecture_section_info";
-
 -- AddForeignKey
 ALTER TABLE "lecture_section_professor" ADD CONSTRAINT "lecture_section_professor_section_id_lecture_id_year_semes_fkey" FOREIGN KEY ("section_id", "lecture_id", "year", "semester") REFERENCES "lecture_section"("id", "lecture_id", "year", "semester") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "bookmark" ADD CONSTRAINT "bookmark_lecture_id_section_id_year_semester_fkey" FOREIGN KEY ("lecture_id", "section_id", "year", "semester") REFERENCES "lecture_section"("id", "lecture_id", "year", "semester") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "bookmark" ADD CONSTRAINT "bookmark_lecture_id_section_id_year_semester_fkey" FOREIGN KEY ("lecture_id", "section_id", "year", "semester") REFERENCES "lecture_section"("lecture_id", "id", "year", "semester") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "record" ADD CONSTRAINT "record_lecture_id_section_id_year_semester_fkey" FOREIGN KEY ("lecture_id", "section_id", "year", "semester") REFERENCES "lecture_section"("lecture_id", "id", "year", "semester") ON DELETE RESTRICT ON UPDATE CASCADE;
