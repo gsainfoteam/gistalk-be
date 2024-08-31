b412a857423d411f5a3fb8623441ba9c6c3fdeca
siwonpada <psw041115@gmail.com>
2024-08-31 19:43:39 +0900

    feat: request Duration

--- src/main.ts
+++ src/main.ts
@@ -58,6 +58,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document, {
     swaggerOptions: {
       oauth2RedirectUrl: `${configService.getOrThrow('API_URL')}/api/oauth2-redirect.html`,
+      displayRequestDuration: true,
     },
   });
   // start server
